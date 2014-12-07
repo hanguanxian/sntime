83281f22a2532ccfd091bb5ac48956a5105dc768
Kail <darkpilgrim@126.com>
2014-12-07 19:00:43 +0800

    set and get rank

--- participated.php
+++ participated.php
@@ -3,7 +3,22 @@
 require 'utils.php';
 initRedbean();
 
-$rank = 1024;
+$rank = R::findOne('rank', ' name = ? ', [ 'rank' ]);
+if (!$rank) {
+    $rank = R::dispense('rank');
+    $rank->name = 'rank';
+    $rank->rank = 1;
+}
+
+// 检查是不是摁手印过来的
+if ($_SERVER['HTTP_REFERER'] != 'http://neone.com.cn/sntime/participate.php') {
+    $rankNum = $rank->rank;
+} else {
+    $rankNum = $rank->rank + 1;
+    $rank->rank = $rankNum;
+    R::store($rank);
+}
+
 render('participated.html.twig', [
-    'rank' => $rank
+    'rank' => $rankNum
 ]);
